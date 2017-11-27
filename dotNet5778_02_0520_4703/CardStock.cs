4e1d178cdba0c4c12c497ef66e0ff96e47882401
David Salem <dsalem9191@gmail.com>
2017-11-27 20:43:05 +0200

    added CardStock class

---
+++ dotNet5778_02_0520_4703/CardStock.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace dotNet5778_02_0520_4703
+{
+    public class CardStock
+    {
+        private List<Cards> Cards;
+
+        public CardStock()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void MixDeck(List<Cards> myCards)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Cards> ToString()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void distribute(params Player[] players)
+        {
+            foreach (Player p in players)
+            {
+                p.AddCards(players.Length,ref Cards)
+            }
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ dotNet5778_02_0520_4703/Player.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace dotNet5778_02_0520_4703
+{
+    public class Player
+    {
+    }
+}
