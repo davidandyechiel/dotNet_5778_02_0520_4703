ac4c0cf58400cae3c7fd3844869372f1bc7e241a
yechiel chabany <Yechel@users.noreply.github.com>
2017-11-27 19:46:18 +0200

    Add project files.

---
+++ dotNet5778_02_0520_4703/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5778_02_0520_4703
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
